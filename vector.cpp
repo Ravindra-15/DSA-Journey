ab734877aa5a1694b3c49993b88e47a3a980ff92
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-08-18 14:56:28 +0530

    Vector Array Printing

--- vector.cpp
+++ vector.cpp
@@ -4,8 +4,33 @@ using namespace std;
 int main() {
     vector <int> arr;
 
-    cout << arr.size() << endl;
-    cout << arr.capacity();
+    cout << arr.size() << endl; // size 
+    cout << arr.capacity();   //capacity
+    cout << endl;
 
+    arr.push_back(5); //pusinh element in array
+    arr.push_back(6);
+    //printing array element
+    for(int i = 0; i < arr.size(); i++){
+        cout <<arr[i] << " ";
+    }
+    cout << endl;
+    arr.pop_back();  //deleting element from array
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i] << endl;
+    }
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << endl;
+    vector<int> brr(n, -1);
+    cout << "Size of brr " << brr.size();
+    cout << endl;
+    cout << "Capacity of brr " << brr.capacity();
+    cout << endl;
+    cout << "brr elements :"<<endl;
+     for(int i = 0; i < brr.size(); i++){
+        cout << brr[i] << " ";
+    }
     return 0;
 }
