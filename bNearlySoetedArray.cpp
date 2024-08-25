5115340becf0c648f45b764e485ce9f67da702f6
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-08-25 16:06:32 +0530

    SearchElement In NearlySortedArray

---
+++ bNearlySoetedArray.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Function to perform binary search
+int findTarget(vector<int> arr, int target)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            return mid;
+        } 
+        // Check if the element just before mid is the target (if it exists)
+        else if (mid - 1 >= 0 && arr[mid - 1] == target)
+        {
+            return mid - 1;
+        }
+        // Check if the element just after mid is the target (if it exists)
+        else if (mid + 1 <= arr.size() && arr[mid + 1] == target)
+        {
+            return mid + 1;
+        }
+        if (target > arr[mid])
+        {
+            start = mid + 2;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> arr{5, 4, 10, 20, 15, 40, 30};
+    int target = 15;
+    int ans = findTarget(arr, target);
+    cout << "Index of " << target << " is " << ans;
+
+    return 0;
+}
