4fb8de71e9b12280cb2f2db2b1a635fc65035a6b
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-10-17 18:52:26 +0530

    Recursion Binary search

---
+++ recursionBinarySearch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int> arr, int s, int e, int target) {
+    int mid = s + (e - s) / 2;  
+
+    //Base Case 1:  
+    if (s > e)
+        return -1;
+
+    //Base Case 2: If element is found at mid, return the index
+    if (arr[mid] == target) {
+        return mid;
+    }
+
+    // // Recursion:  
+    // if (arr[mid] < target) {
+    //     int ans = binarySearch(arr, mid + 1, e, target);
+    //     return ans;  // return binarySearch(arr, mid + 1, e, target);
+    // }
+
+    // if (arr[mid] > target) {
+    //     int ans = binarySearch(arr, s, mid - 1, target);
+    //     return ans;
+    // }
+
+   // Recursion using ternary operator
+    return (arr[mid] < target) 
+        ? binarySearch(arr, mid + 1, e, target)  // Search in the right half
+        : binarySearch(arr, s, mid - 1, target);  // Search in the left half
+        
+}
+
+int main() {
+    vector<int> arr{10, 20, 30, 40, 50};  
+    int target = 40;  // Element to find
+    int n = arr.size();  // Size of the array
+    int s = 0;  // Start index
+    int e = n - 1;  // End index
+ 
+    int ans = binarySearch(arr, s, e, target);
+    
+    cout << "Target found at index: " << ans << endl;
+
+    return 0;
+}
