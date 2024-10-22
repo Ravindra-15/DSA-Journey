7d2b94d2899b94c2b35af71fbaf98b68246f2961
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-10-22 17:56:48 +0530

    string reverse recursion

---
+++ recReverseString.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+void solve(string& name, int start, int end){
+    if(start >= end){
+        return;
+    }
+
+    swap(name[start], name[end]);
+    solve(name, start + 1 , end - 1);
+}
+int main() {
+    string name;
+    cout << "Enter a string  : ";
+    cin >> name;
+    int n = name.length();
+    int start = 0;
+    int end = n-1;
+    solve(name, start, end);
+    cout << "The reverse string is : "<< name;
+    return 0;
+}
